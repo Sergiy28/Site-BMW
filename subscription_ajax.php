67214e316bab5282255ec38d3945d08e845d1098
Sergiy28 <sergbalan2803@gmail.com>
2024-04-30 21:31:53 +0300

    ten

--- subscription_ajax.php
+++ subscription_ajax.php
@@ -1,4 +1,9 @@
 <?php  
+// Встановлюємо заголовки CORS
+header("Access-Control-Allow-Origin: http://localhost");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 require 'src/subscription.php';  
 // Додаєму нову підписку з даних з POST
  addSubscription($_POST); 
